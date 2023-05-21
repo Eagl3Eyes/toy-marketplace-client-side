21692610a581a21bf18e71f0c0cd2b765f88ea5d
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-05-21 23:30:31 +0600

    style change

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -66,8 +66,8 @@ const Login = () => {
                 <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
                     {/* <!-- form --> */}
                     <div className="md:w-1/2 px-8 md:px-16">
-                        <h2 className="font-bold text-2xl text-[#002D74]">Login</h2>
-                        <p className="text-xs mt-4 text-[#002D74]">If you are already a member, easily log in</p>
+                        <h2 className="font-bold text-2xl text-[#ED1C24]">Login</h2>
+                        <p className="text-xs mt-4 text-[#ED1C24]">If you are already a member, easily log in</p>
 
                         <form onSubmit={login} className="flex flex-col gap-4">
                             <input className="p-2 mt-8 rounded-xl border" id="email" type="email" name="email" placeholder="Email" required />
@@ -78,7 +78,7 @@ const Login = () => {
                                     <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
                                 </svg>
                             </div>
-                            <button className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300">Login</button>
+                            <button className="bg-[#ED1C24] rounded-xl text-white py-2 hover:scale-105 duration-300">Login</button>
                         </form>
 
                         <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
@@ -87,21 +87,21 @@ const Login = () => {
                             <hr className="border-gray-400" />
                         </div>
 
-                        <button onClick={() => { singIn() }} className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#002D74]">
+                        <button onClick={() => { singIn() }} className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#ED1C24]">
                             <FcGoogle size={35} />
                             Login with Google
                         </button>
 
-                        <button onClick={() => { github() }} className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#002D74]">
+                        <button onClick={() => { github() }} className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#ED1C24]">
                             <IoLogoGithub size={35} />
                             Login with Github
                         </button>
 
-                        <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
+                        <div className="mt-5 text-xs border-b border-[#ED1C24] py-4 text-[#ED1C24]">
                             <a href="#">Forgot your password?</a>
                         </div>
 
-                        <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
+                        <div className="mt-3 text-xs flex justify-between items-center text-[#ED1C24]">
                             <p>Dont have an account?</p>
                             <Link to="/register" className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Register</Link>
                         </div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -63,8 +63,8 @@ export default function Register() {
                 <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
                     {/* <!-- form --> */}
                     <div className="md:w-1/2 px-8 md:px-16">
-                        <h2 className="font-bold text-2xl text-[#002D74]">Register</h2>
-                        <p className="text-xs mt-4 text-[#002D74]">Please provide the information!</p>
+                        <h2 className="font-bold text-2xl text-[#ED1C24]">Register</h2>
+                        <p className="text-xs mt-4 text-[#ED1C24]">Please provide the information!</p>
 
                         <form onSubmit={register} className="flex flex-col gap-4">
                             <input className="p-2 mt-8 rounded-xl border" id="name" type="text" name="name" placeholder="Name" required />
@@ -77,15 +77,15 @@ export default function Register() {
                                     <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
                                 </svg>
                             </div>
-                            <button className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300">Login</button>
+                            <button className="bg-[#ED1C24] rounded-xl text-white py-2 hover:scale-105 duration-300">Login</button>
                         </form>
 
 
-                        <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
+                        <div className="mt-5 text-xs border-b border-[#ED1C24] py-4 text-[#ED1C24]">
                             <a href="#">Forgot your password?</a>
                         </div>
 
-                        <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
+                        <div className="mt-3 text-xs flex justify-between items-center text-[#ED1C24]">
                             <p>Already have an account?</p>
                             <Link to="/login" className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Login</Link>
                         </div>
